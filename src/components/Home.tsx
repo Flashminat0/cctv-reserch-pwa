f4b918d45437d9625ec4fdc8a938403627734f67
Yasas Lowe <it20014940@my.sliit.lk>
2023-05-23 04:20:13 +0530

    auth system done

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,61 +1,14 @@
 import styles from '@styles/Home.module.css';
-import { getAuth, GoogleAuthProvider, signInWithRedirect } from 'firebase/auth';
+import { getAuth, getRedirectResult, GoogleAuthProvider, signInWithRedirect } from 'firebase/auth';
 import { initializeApp } from 'firebase/app';
 import { useEffect, useState } from 'react';
 import { firebaseConfig } from '../../firebase';
+import Link from 'next/link';
 
 function Home(): JSX.Element {
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
+  // const app = initializeApp(firebaseConfig);
+  // const auth = getAuth(app);
 
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [buttonText, setButtonText] = useState('Sign In');
-
-  useEffect(() => {
-    if (loggedIn) {
-      setButtonText('Signed In');
-    } else if (loading) {
-      setButtonText('Loading...');
-    } else {
-      setButtonText('Sign In');
-    }
-  }, [loggedIn, loading]);
-
-  const signInWithGoogle = () => {
-    setLoading(true);
-    const provider = new GoogleAuthProvider();
-
-    provider.setCustomParameters({
-      // Force re-consent.
-      prompt: 'consent',
-    });
-
-    signInWithRedirect(auth, provider)
-      .then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential?.accessToken;
-        // The signed-in user info.
-        const { user } = result;
-        // IdP data available using getAdditionalUserInfo(result)
-        // ...
-        setLoggedIn(true);
-        setLoading(false);
-        console.log(user);
-      })
-      .catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const { email } = error.customData;
-        // The AuthCredential type that was used.
-        const credential = GoogleAuthProvider.credentialFromError(error);
-        // ...
-        setLoading(false);
-      });
-  };
 
   return (
     <div className={styles.containers}>
@@ -63,18 +16,15 @@ function Home(): JSX.Element {
       <br />
       <h2 className={styles.content}>Let&apos;s get started by signing in with your SLIIT email</h2>
       <br />
-      <div className="mt-10">
-        <button
-          type="button"
-          onKeyDown={signInWithGoogle}
-          onClick={signInWithGoogle}
-          className={`group ${styles.link}`}
+      <div className='mt-10'>
+        <Link href='/auth'
+              className={`group ${styles.link}`}
         >
           <span className={styles.border} />
           <span className={`group-hover:bg-opacity-0 duration-400 ${styles.btn}`}>
-            <span className={styles.text}>{buttonText}</span>
+            <span className={styles.text}>Sign in</span>
           </span>
-        </button>
+        </Link>
       </div>
     </div>
   );
--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,93 +1,126 @@
 import Page from '@layouts/Page';
 import Section from '@layouts/Section';
 import { Home } from '@components/index';
+import React, { useState } from 'react';
+import { firebaseConfig } from '../../firebase';
+import { initializeApp } from 'firebase/app';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
 export default function main(): JSX.Element {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+
+  const [verifiedEmail, setVerifiedEmail] = useState('');
+
+  const formSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        if (userCredential?.user?.email) {
+          setVerifiedEmail(userCredential?.user?.email);
+        }
+        
+      }).catch((error) => {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          if (userCredential?.user?.email) {
+            setVerifiedEmail(userCredential?.user?.email);
+          }
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // ..
+        });
+    });
+  };
+
   return (
-    <Page title="Home">
-      <Section>
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+    <Page title='Home'>
+      <Section className={` `}>
+        <div className='flex min-h-full flex-1 flex-col justify-center px-6 pt-40 lg:px-8'>
+          <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
             <img
-              className="mx-auto h-10 w-auto"
-              src="/icons/icon-maskable-192x192.png"
-              alt="Your Company"
+              className='mx-auto h-10 w-auto'
+              src='/icons/icon-maskable-192x192.png'
+              alt='Your Company'
             />
             <h2
-              className="mt-10  text-2xl font-bold leading-9 tracking-tight
+              className='mt-10  text-2xl font-bold leading-9 tracking-tight
             text-center animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent
-            "
+            '
             >
               Sign in to your account
             </h2>
           </div>
 
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" action="#" method="POST">
+          <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
+            <form className='space-y-6' onSubmit={formSubmit}>
               <div>
                 <label
-                  htmlFor="email"
-                  className="block text-sm font-medium leading-6 text-gray-900"
+                  htmlFor='email'
+                  className='block text-sm font-medium leading-6 text-indigo-100'
                 >
                   Email address
                 </label>
-                <div className="mt-2">
+                <div className='mt-2'>
                   <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    id='email'
+                    name='email'
+                    type='email'
+                    autoComplete='email'
                     required
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                   />
                 </div>
               </div>
 
               <div>
-                <div className="flex items-center justify-between">
+                <div className='flex items-center justify-between'>
                   <label
-                    htmlFor="password"
-                    className="block text-sm font-medium leading-6 text-gray-900"
+                    htmlFor='password'
+                    className='block text-sm font-medium leading-6 text-indigo-100'
                   >
                     Password
                   </label>
-                  <div className="text-sm">
-                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                  <div className='text-sm'>
+                    <a href='#' className='font-semibold text-indigo-100 hover:text-indigo-500'>
                       Forgot password?
                     </a>
                   </div>
                 </div>
-                <div className="mt-2">
+                <div className='mt-2'>
                   <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    id='password'
+                    name='password'
+                    type='password'
+                    autoComplete='current-password'
                     required
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                   />
                 </div>
               </div>
 
               <div>
                 <button
-                  type="submit"
-                  className="
+                  type='submit'
+                  className='
                   animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500
                   flex w-full justify-center rounded-md  px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm
-                   focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 "
+                   focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 '
                 >
                   Sign in
                 </button>
               </div>
             </form>
-
-            <p className="mt-10 text-center text-sm text-gray-500">
-              Not a member?{' '}
-              <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                Start a 14 day free trial
-              </a>
-            </p>
           </div>
         </div>
       </Section>
